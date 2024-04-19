434a131ec904bb54384bad00fbc77a163e228e41
Alonso del Arte <alonso.delarte@gmail.com>
2024-04-19 15:24:53 -0400

    Various groundwork

---
+++ TicTacToe/Board.swift
@@ -0,0 +1,17 @@
+//
+//  Board.swift
+//  TicTacToe
+//
+//  Created by Alonso del Arte on 4/19/24.
+//
+
+import Foundation
+
+struct Board {
+    
+    init(_ playerA: Player, _ playerB: Player, horiz: UInt8 = 3, 
+         vertic: UInt8 = 3) {
+        //
+    }
+    
+}
---
+++ TicTacToe/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  TicTacToe
+//
+//  Created by Alonso del Arte on 4/19/24.
+//
+
+import Foundation
+
+struct Player {
+    
+    init(name: String, token: Character) {
+        //
+    }
+    
+}
